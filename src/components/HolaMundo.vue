f86339f1e9251ed22f15e339ad6b58baee7d35ae
Javierrocadev <javierroca.dev@gmail.com>
2023-10-27 12:49:28 +0200

    VUEPRIMER

---
+++ src/components/HolaMundo.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h1>Mi primera app vue</h1>
+        <h2>su nombre es : {{nombre}}</h2>
+        <img src="./../assets/galaxy.jpg">
+    </div>
+</template>
+<script>
+    //siempre debemos exportar el nombre que deseemos 
+    //dar al component en nuestra apicacion vue
+    export default{
+        name:"HolaMundo",
+        data(){
+            return {
+                nombre:"alumno",
+                numero :12,
+                miarray:[],
+                objeto:{}
+            }
+        }
+    }
+</script>
+<style>
+    h1{
+        color:rgb(38, 93, 165)
+    }
+    @import './../assets/css/estilos.css'
+</style>
